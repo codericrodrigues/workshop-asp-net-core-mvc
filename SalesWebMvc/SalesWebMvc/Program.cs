6d3567a556978cf9867fe6b8ff791738a5c1c985
Eric Rodrigues <codericrodrigues@outlook.com>
2022-02-01 00:48:38 -0300

    teste

--- SalesWebMvc/SalesWebMvc/Program.cs
+++ SalesWebMvc/SalesWebMvc/Program.cs
@@ -15,6 +15,7 @@ namespace SalesWebMvc
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            Console.WriteLine("Olá tudo bem!");
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
